bf61d47c0c1eb84cf1b89a04175a3b9be1b0f7ac
ghostgoal <hoopxxl@163.com>
2015-04-09 17:31:56 +0800

    1.2
    配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+var def_config = [{
+		name : '.tlistrow .tlistname',
+		download : 'tlistrow .tlistdownload',
+		size : 'tlistrow .tlistsize',
+
+	}, {
+		name : '.i_info',
+		download : '.j_size a',
+		size : 'j_size'
+	}
+
+];
